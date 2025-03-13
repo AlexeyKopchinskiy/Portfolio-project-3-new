e0d80f01ef00c390447e76585dd59843cc86ce35
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-03-13 13:24:54 +0100

    Update the main function. Integrate the mark_task_completed functionality

--- run.py
+++ run.py
@@ -362,7 +362,8 @@ def main():
         print("3 - View tasks list")
         print("4 - Update a task")
         print("5 - Delete (archive) a task")
-        print("6 - Exit")
+        print("6 - Mark a task as completed")
+        print("7 - Exit")
 
         # Get the user's choice
         user_choice = input("Enter the number of your choice: ").strip()
@@ -378,6 +379,8 @@ def main():
         elif user_choice == "5":
             delete_task()
         elif user_choice == "6":
+            mark_task_completed()
+        elif user_choice == "7":
             print("Exiting the Task Manager. Have a great day!")
             break
         else:
